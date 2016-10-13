4b2fa18b9ff5f4c8618a8cd2f0fbe232843466ff
Koen Lageveen <koen.lageveen@me.com>
2016-10-13 12:22:02 +0200

    regex

--- publish.sh
+++ publish.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+set -e
+
+NOW=`date +"%Y-%m-%d %T"`
+
+zip -rq sublime.zip src
+
+sed "s/VERSION/${CI_BUILD_TAG}/g" fabric-sublime-repository-template.json > fabric-sublime-repository-interm.json
+sed "s/DATE/${NOW}/g" fabric-sublime-repository-interm.json > fabric-sublime-repository.json
+
+scp pruts.nl:websites/packages.m-industries.com/sublime/sublime.zip sublime.zip
+scp pruts.nl:websites/packages.m-industries.com/sublime/fabric-sublime-repository.json fabric-sublime-repository.json
