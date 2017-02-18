1cbe5f57efb851cc360878e2497fbbbbc4644d6b
Koen Lageveen <klageveen@m-industries.com>
2017-02-18 17:19:03 +0100

    Update publish.sh urls

--- publish.sh
+++ publish.sh
@@ -8,5 +8,5 @@ zip -rq sublime.zip src
 sed "s/VERSION/${CI_BUILD_TAG}/g" fabric-sublime-repository-template.json > fabric-sublime-repository-interm.json
 sed "s/DATE/${NOW}/g" fabric-sublime-repository-interm.json > fabric-sublime-repository.json
 
-scp sublime.zip pruts.nl:websites/packages.m-industries.com/sublime/sublime.zip
-scp fabric-sublime-repository.json pruts.nl:websites/packages.m-industries.com/sublime/fabric-sublime-repository.json
+scp sublime.zip pruts.nl:websites/m-industries.com/sublime/sublime.zip
+scp fabric-sublime-repository.json pruts.nl:websites/m-industries.com/sublime/fabric-sublime-repository.json
