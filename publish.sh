3479f5af3bf24a6db147579176ef32e5a1a8beea
Koen Lageveen <koen.lageveen@me.com>
2017-06-16 16:59:33 +0200

    move syntaxes up a folder so build is always in the same place

--- publish.sh
+++ publish.sh
@@ -3,9 +3,9 @@ set -e
 
 NOW=`date +"%Y-%m-%d %T"`
 
-pushd src >> /dev/null
-	zip -rq ../sublime.zip .
-popd >> /dev/null
+zip -rq sublime.zip Syntaxes
+zip -rq sublime.zip comments.tmPreferences
+zip -rq sublime.zip indent.tmPreferences
 
 sed "s/VERSION/${CI_BUILD_TAG}/g" fabric-sublime-repository-template.json > fabric-sublime-repository-interm.json
 sed "s/DATE/${NOW}/g" fabric-sublime-repository-interm.json > fabric-sublime-repository.json
