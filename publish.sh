02bd4e80a55d2bdaa571d914b97b4b62043d9d31
Koen Lageveen <koen.lageveen@me.com>
2017-07-06 16:44:26 +0200

    update publication for new https server

--- publish.sh
+++ publish.sh
@@ -10,5 +10,5 @@ zip -rq sublime.zip indent.tmPreferences
 sed "s/VERSION/${CI_BUILD_TAG}/g" fabric-sublime-repository-template.json > fabric-sublime-repository-interm.json
 sed "s/DATE/${NOW}/g" fabric-sublime-repository-interm.json > fabric-sublime-repository.json
 
-scp sublime.zip corno@pruts.nl:websites/m-industries.com/sublime/sublime.zip
-scp fabric-sublime-repository.json corno@pruts.nl:websites/m-industries.com/sublime/fabric-sublime-repository.json
+scp sublime.zip alan@office.m-industries.com:sites/www.m-industries.com/sublime/sublime.zip
+scp fabric-sublime-repository.json alan@office.m-industries.com:sites/www.m-industries.com/sublime/fabric-sublime-repository.json
